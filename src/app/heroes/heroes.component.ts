e6e707ece669f4b06fbca471b31a3a002bfd432e
Luiz Henrique Camargo <luiz.henriquecamargo@hotmail.com>
2021-05-04 11:27:00 -0300

    Part 2 finished

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,17 +8,16 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Luiz Henrique Alberto de Camargo',
-    idade: 20,
-    cidade:'Porto Feliz',
-    instituicao: 'Fatec Itu - Dom Amaury Castanho'
-  };
+
+  heroes = HEROES;
+  selectedHero?: Hero;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Luiz Henrique', idade: 20, cidade: 'Porto Feliz', instituicao: 'Fatec' },
+  { id: 12, name: 'Marcos', idade: 21, cidade: 'Itu', instituicao: 'Fatec' },
+  { id: 13, name: 'Bruno', idade: 19, cidade: 'Jaú', instituicao: 'Fatec' },
+  { id: 14, name: 'Caleb', idade: 20, cidade: 'Sorocaba', instituicao: 'Fatec' },
+  { id: 15, name: 'Maria Fernanda', idade: 21, cidade: 'Piracicaba', instituicao: 'Fatec' },
+  { id: 16, name: 'Ruth', idade: 22, cidade: 'Porto Feliz', instituicao: 'Fatec' },
+  { id: 17, name: 'Daniel', idade: 23, cidade: 'Itu', instituicao: 'Fatec' },
+  { id: 18, name: 'Fernando', idade: 18, cidade: 'Sorocaba', instituicao: 'Fatec' },
+  { id: 19, name: 'Matheus', idade: 20, cidade: 'Alumínio', instituicao: 'Fatec' },
+  { id: 20, name: 'Alice', idade: 21, cidade: 'Itu', instituicao: 'Fatec' }
+];
